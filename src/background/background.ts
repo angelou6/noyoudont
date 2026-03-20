5c4bc4756ba044677ffacd00ac55de6683df33e5
Angelo <elpepechingon@outlook.com>
2026-03-19 23:44:19 -0600

    arreglando loop infinito

--- src/background/background.ts
+++ src/background/background.ts
@@ -3,10 +3,11 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     browser.storage.local.get(["urls"]).then((res) => {
       const urls = res.urls ?? [];
       for (const url of urls) {
-        if (tab.url?.includes(url) && tab.id)
+        if (tab.url?.includes(url) && tab.id) {
           browser.tabs.update(tab.id, {
             url: browser.runtime.getURL("src/no/no.html"),
           });
+        }
       }
     });
   }
